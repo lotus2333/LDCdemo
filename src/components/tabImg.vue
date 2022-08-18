690b26e7a3a31e0197f238f5ef6e3665bae0ecc5
lotus2333 <95925652+lotus2333@users.noreply.github.com>
2022-08-18 21:48:05 +0800

    Add files via upload

---
+++ src/components/tabImg.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <img src="../5.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+img{
+  width: 148px;
+  height: 148px;
+  border-radius: 10px;
+}
+</style>
